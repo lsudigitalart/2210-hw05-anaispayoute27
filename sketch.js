84d15080fe83afbbd5b6a7000f0f2d786d35bcb0
Anais Payoute <apayou1 @lsu.edu>
2024-10-09 13:09:23 -0500

    Drawing thing

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(0, 0, 0);
+  stroke(0, 0, 0);
+  line(365, 35, 365, 365)
+  line(365, 35, 35, 35)
+  line(35, 35, 35, 365)
+  line(35, 365, 365, 365)
+
+  fill(139, 0, 0);
+  stroke(139, 0, 0);
+  line(35, 200, 265, 200)
+}
